797252d0ddb25a53d1610f43c505c7e5f1085179
cavila31 <cavila31@hotmail.com>
2016-06-20 12:01:44 -0600

    Cliente casi listo

---
+++ ECCI_Gato/Juego.aspx.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+
+namespace ECCI_Gato
+{
+    public partial class Juego : System.Web.UI.Page
+    {
+        private int clickedByPlayerNo, clickNo;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Limpiartablero(false);
+        }
+
+        protected void NuevoJuego(object sender, EventArgs e)
+        {
+            Limpiartablero(true);
+            clickNo = 0;
+        }
+
+        protected void Limpiartablero(bool a)
+        {
+            Button1.Text = "";
+            Button1.Enabled = a;
+            Button2.Text = "";
+            Button2.Enabled = a;
+            Button3.Text = "";
+            Button3.Enabled = a;
+            Button4.Text = "";
+            Button4.Enabled = a;
+            Button5.Text = "";
+            Button5.Enabled = a;
+            Button6.Text = "";
+            Button6.Enabled = a;
+            Button7.Text = "";
+            Button7.Enabled = a;
+            Button8.Text = "";
+            Button8.Enabled = a;
+            Button0.Text = "";
+            Button0.Enabled = a;
+        }
+        protected void btn0_Click(object sender, EventArgs e)
+        {
+            int[] coordinadas = { 0, 0 };
+            movida(sender, coordinadas);
+        }
+
+        protected void btn1_Click(object sender, EventArgs e)
+        {
+            int[] coordinadas = { 0, 1 };
+            movida(sender, coordinadas);
+        }
+
+        protected void btn2_Click(object sender, EventArgs e)
+        {
+            int[] coordinadas = { 0, 2 };
+            movida(sender, coordinadas);
+        }
+
+        protected void btn3_Click(object sender, EventArgs e)
+        {
+            int[] coordinadas = { 1, 0 };
+            movida(sender, coordinadas);
+        }
+
+        protected void btn4_Click(object sender, EventArgs e)
+        {
+            int[] coordinadas = { 1, 1 };
+            movida(sender, coordinadas);
+        }
+
+        protected void btn5_Click(object sender, EventArgs e)
+        {
+            int[] coordinadas = { 1, 2 };
+            movida(sender, coordinadas);
+        }
+
+        protected void btn6_Click(object sender, EventArgs e)
+        {
+            int[] coordinadas = { 2, 0 };
+            movida(sender, coordinadas);
+        }
+
+        protected void btn7_Click(object sender, EventArgs e)
+        {
+            int[] coordinadas = { 2, 1 };
+            movida(sender, coordinadas);
+        }
+
+        protected void btn8_Click(object sender, EventArgs e)
+        {
+            int[] coordinadas = { 2, 2 };
+            movida(sender, coordinadas);
+        }
+
+        private void movida(object Sender, int[] coordinadas)
+        {
+            clickNo += 1; //Increase no. of clicks every time a button is clicked
+
+            //All odd numbered clicks are done by 1st player and all even numbered clicks are done by 2nd player.
+            if (clickNo % 2 == 1)
+                clickedByPlayerNo = 1;
+            else
+                clickedByPlayerNo = 2;
+
+
+            //for 1st player, assign 'X' and for 2nd player assign '0'
+            if (clickedByPlayerNo == 1)
+                ((Button)Sender).Text = "X";
+            else
+                ((Button)Sender).Text = "0";
+            ((Button)Sender).Enabled = false; //Disable the button once it is clicked
+
+            revisarGanador();
+        }
+
+        private void revisarGanador()
+        {
+            if (clickNo > 4)
+            {
+
+            }
+        }
+
+        private void desplegarGanador()
+        {
+            int x = 0;
+
+            if (x == 0)
+            {
+
+                ScriptManager.RegisterStartupScript(this, GetType(), "Fue Un Empate", "alertMessage();", true);
+
+            }
+            if (x == 1)
+            {
+
+                ScriptManager.RegisterStartupScript(this, GetType(), "El ganador fue: ", "alertMessage();", true);
+            }
+
+            if (x == -1)
+            {
+
+                ScriptManager.RegisterStartupScript(this, GetType(), "El ganador fue: ", "alertMessage();", true);
+
+            }
+
+        }
+
+        protected void guardarGanador(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void Top10()
+        {
+
+        }
+}
