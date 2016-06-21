61ac244683ab9fb17d3561a10c4df39a73272d84
cavila31 <cavila31@hotmail.com>
2016-06-21 11:49:12 -0600

    .

--- ECCI_Gato/Juego.aspx.cs
+++ ECCI_Gato/Juego.aspx.cs
@@ -46,7 +46,7 @@ namespace ECCI_Gato
             {
                 ((Button)Sender).Text = "O";
             }
-          //  ((Button)Sender).Enabled = false; //Disable the button once it is clicked
+            ((Button)Sender).Enabled = false; //Disable the button once it is clicked
             string arreglo = coordinadas[0].ToString() + "," + coordinadas[1].ToString() + "," + gato.jugadorActual();
             gato.mover(arreglo);
             revisarGanador();
